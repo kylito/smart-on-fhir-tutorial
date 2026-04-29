8295309e38b175e5e6b7564e99a0afee59456c7e
kylito <kylito@users.noreply.github.com>
2026-04-29 14:34:28 -0400

    update

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -21,7 +21,8 @@
                         $or: ['http://loinc.org|8302-2',
                               'http://loinc.org|2085-9',
                               'http://loinc.org|2089-1',
-                              'http://loinc.org|85354-9']
+                              'http://loinc.org|85354-9',
+                              'http://loinc.org|8310-5']
                       }
                     }
                   });
@@ -48,6 +49,7 @@
           var diastolicbp = getBloodPressureValue(byCodes('85354-9'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
+          var bodytemp = byCodes('8310-5');
 
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
